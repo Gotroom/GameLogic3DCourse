52143bbcfac3cfb4986a95d3afd23e3c844a6b62
DenisErmolaev <ermolaev.vt@gmail.com>
2020-05-12 10:01:33 +0300

    - selection controller fix

--- Assets/Scripts/GameLogic/Controller/SelectionController.cs
+++ Assets/Scripts/GameLogic/Controller/SelectionController.cs
@@ -5,6 +5,7 @@ namespace Ermolaev_3D
 {
     public sealed class SelectionController : BaseController, IExecutable
     {
+        private readonly Camera _mainCamera;
         private readonly Vector2 _center;
         private readonly float _dedicateDistance = 20.0f;
         private GameObject _dedicatedObj;
@@ -14,13 +15,14 @@ namespace Ermolaev_3D
 
         public SelectionController()
         {
+            _mainCamera = Camera.main;
             _center = new Vector2(Screen.width / 2.0f, Screen.height / 2.0f);
         }
 
         public void Execute()
         {
             if (!IsActive) return;
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(_center), 
+            if (Physics.Raycast(_mainCamera.ScreenPointToRay(_center), 
                 out var hit, _dedicateDistance))
             {
                 SelectObject(hit.collider.gameObject);
